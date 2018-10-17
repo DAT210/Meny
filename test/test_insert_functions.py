c978fc1035199969e71a75caea207a553eb51589
magn00bs <magnus@steinsto.net>
2018-10-17 19:31:28 +0200

    Implemented functions for new selection table

--- test/test_insert_functions.py
+++ test/test_insert_functions.py
@@ -30,14 +30,15 @@ class TestInsertFunctions(unittest.TestCase):
     def test_insert_course(self):
         db = get_db()
         # Valid input
-        insert_course(db, "insert course", 2, 3.67)
+        insert_course(db, "insert course", 2, "info insert course", 3.67)
         cur = db.cursor()
         try:
-            cur.execute("SELECT c_name, ca_id, price FROM course WHERE c_id = (SELECT MAX(c_id) FROM course)")
+            cur.execute("SELECT c_name, ca_id, info, price FROM course WHERE c_id = (SELECT MAX(c_id) FROM course)")
             values = cur.fetchone()
             name = values[0]
             category = values[1]
-            price = values[2]
+            info = values[2]
+            price = values[3]
         except Error as err:
             return err
         finally:
@@ -45,38 +46,43 @@ class TestInsertFunctions(unittest.TestCase):
 
         self.assertEqual(name, "insert course")
         self.assertEqual(category, 2)
+        self.assertEqual(info, "info insert course")
         self.assertEqual(str(price), str(3.67))
 
         # Insert invalid name
-        self.assertEqual(insert_course(db, "asdfasdfasdfasdfasdfasdfasdfasdfasfdasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf", 2, 4.56),
+        self.assertEqual(insert_course(db, "asdfasdfasdfasdfasdfasdfasdfasdfasfdasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf", 2, "info for course", 4.56),
                          INPUT_TOO_LONG_EXCEPTION)
 
         # Insert with invalid category
-        self.assertEqual(insert_course(db, "course cat inv", "a", 7.86),
+        self.assertEqual(insert_course(db, "course cat inv", "a", "info for course", 7.86),
                          INVALID_TYPE_EXCEPTION)
 
+        # Insert invalid info
+        self.assertEqual(insert_course(db, "course valid", 2, "asdfasdfasdfasdfasdfasdfasdfasdfasfdasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasfdasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasfdasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf", 4.56),
+                         INPUT_TOO_LONG_EXCEPTION)
+
         # Insert invalid price
-        self.assertEqual(insert_course(db, "course with valid name", 3, "a"),
+        self.assertEqual(insert_course(db, "course with valid name", 3, "info for course", "a"),
                          INVALID_DECIMAL_VALUE)
 
         # Insert name to existing name (name must be unique)
-        self.assertEqual(insert_course(db, "course alpha", 4, 2.13),
+        self.assertEqual(insert_course(db, "course alpha", 4, "info for course", 2.13),
                          DUPLICATE_VALUE_EXCEPTION)
 
         # Insert category with non-existing id
-        self.assertEqual(insert_course(db, "course cat none", 999, 3.21),
+        self.assertEqual(insert_course(db, "course cat none", 999, "info for course", 3.21),
                          UNKKNOWN_REFERENCE_EXCEPTION)
 
         # Insert with empty name value
-        self.assertEqual(insert_course(db, None, 1, 5.78),
+        self.assertEqual(insert_course(db, None, 1, "info for course", 5.78),
                          EMPTY_INPUT_EXCEPTION)
 
         # Insert with empty category
-        self.assertEqual(insert_course(db, "course cat empty", None, 5.97),
+        self.assertEqual(insert_course(db, "course cat empty", None, "info for course", 5.97),
                          EMPTY_INPUT_EXCEPTION)
 
         # Insert with empty price
-        self.assertEqual(insert_course(db, "course unique name", 2, None),
+        self.assertEqual(insert_course(db, "course unique name", 2, "info for course", None),
                          EMPTY_INPUT_EXCEPTION)
         db.close()
 
@@ -222,6 +228,131 @@ class TestInsertFunctions(unittest.TestCase):
         self.assertEqual(insert_course_ingredient(db, 3, None),
                          EMPTY_INPUT_EXCEPTION)
         db.close()
+
+
+    def test_insert_selection_category(self):
+        db = get_db()
+        # Valid input
+        insert_selection_category(db, "insert selection category")
+        cur = db.cursor()
+        try:
+            cur.execute("SELECT sc_name FROM category WHERE sc_id = (SELECT MAX(sc_id) FROM category)")
+            category = cur.fetchone()[0]
+        except Error as err:
+            return err
+        finally:
+            cur.close()
+
+        self.assertEqual(category, "insert selection category")
+
+        # Insert invalid name
+        self.assertEqual(insert_selection_category(db,
+                                          "asdfasdfasdfasdfasdfasdfasdfasdfasfdasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf"),
+                         INPUT_TOO_LONG_EXCEPTION)
+
+        # Insert name to existing name (name must be unique)
+        self.assertEqual(insert_selection_category(db, "selection category alpha"),
+                         DUPLICATE_VALUE_EXCEPTION)
+
+        # Insert with empty name value
+        self.assertEqual(insert_selection_category(db, None),
+                         EMPTY_INPUT_EXCEPTION)
+        db.close()
+
+
+    def test_insert_selection(self):
+        db = get_db()
+        # Valid input
+        insert_selection(db, "insert selection", 2, 2)
+        cur = db.cursor()
+        try:
+            cur.execute("SELECT s_name, sc_id, i_id FROM selection WHERE s_id = (SELECT MAX(s_id) FROM selection)")
+            values = cur.fetchone()
+            name = values[0]
+            category = values[1]
+            ingredient = values[2]
+        except Error as err:
+            return err
+        finally:
+            cur.close()
+
+        self.assertEqual(name, "insert selection")
+        self.assertEqual(category, 2)
+        self.assertEqual(ingredient, 2)
+
+        # Insert invalid name
+        self.assertEqual(insert_selection(db, "asdfasdfasdfasdfasdfasdfasdfasdfasfdasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf", 1, 1),
+                         INPUT_TOO_LONG_EXCEPTION)
+
+        # Insert with invalid category
+        self.assertEqual(insert_selection(db, "selection cat inv", "a", 3),
+                         INVALID_TYPE_EXCEPTION)
+
+        # Insert with invalid ingredient
+        self.assertEqual(insert_selection(db, "selection cat inv", 2, "a"),
+                         INVALID_TYPE_EXCEPTION)
+
+        # Insert name to existing name (name must be unique)
+        self.assertEqual(insert_selection(db, "selection alpha", 1, 4),
+                         DUPLICATE_VALUE_EXCEPTION)
+
+        # Insert category with non-existing selection category id
+        self.assertEqual(insert_selection(db, "selection cat none", 999, 4),
+                         UNKKNOWN_REFERENCE_EXCEPTION)
+
+        # Insert with empty name value
+        self.assertEqual(insert_selection(db, None, 1, 3),
+                         EMPTY_INPUT_EXCEPTION)
+
+        # Insert with empty category
+        self.assertEqual(insert_selection(db, "selection cat empty", None, 3),
+                         EMPTY_INPUT_EXCEPTION)
+        db.close()
+
+
+    def test_insert_course_selection(self):
+        db = get_db()
+        # Valid input
+        insert_course_selection(db, 3, 2)
+        cur = db.cursor()
+        try:
+            cur.execute("SELECT c_id, s_id FROM course_selection WHERE i_id = 3 AND a_id = 2")
+            values = cur.fetchone()
+            course = values[0]
+            selection = values[1]
+        except Error as err:
+            return err
+        finally:
+            cur.close()
+
+        self.assertEqual(course, 3)
+        self.assertEqual(selection, 2)
+
+        # Insert invalid course reference
+        self.assertEqual(insert_course_selection(db, 999, 3),
+                         UNKKNOWN_REFERENCE_EXCEPTION)
+
+        # Insert invalid selection reference
+        self.assertEqual(insert_course_selection(db, 3, 999),
+                         UNKKNOWN_REFERENCE_EXCEPTION)
+
+        # Insert invalid course value
+        self.assertEqual(insert_course_selection(db, "a", 3),
+                         INVALID_TYPE_EXCEPTION)
+
+        # Insert invalid selection value
+        self.assertEqual(insert_course_selection(db, 3, "a"),
+                         INVALID_TYPE_EXCEPTION)
+
+        # Insert with empty course value
+        self.assertEqual(insert_course_selection(db, None, 3),
+                         EMPTY_INPUT_EXCEPTION)
+
+        # Insert with empty selection value
+        self.assertEqual(insert_course_selection(db, 4, None),
+                         EMPTY_INPUT_EXCEPTION)
+        db.close()
+
 
     def test_ingredient_allergene(self):
         db = get_db()
