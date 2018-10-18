00fed93cb09b695a6cbd9e4e4d86a45f8d371fcb
magn00bs <magnus@steinsto.net>
2018-10-18 20:13:45 +0200

    Added functions for category and selection

--- src/get_functions.py
+++ src/get_functions.py
@@ -6,7 +6,7 @@ from mysql.connector import IntegrityError, DataError, Error
 
 get_queries = {
     # Get all courses
-    "get_courses": "SELECT c_id, c_name, price FROM course",
+    "get_courses": "SELECT c_id, c_name, ca_id, info, price FROM course",
 
     # Get course ingredients by c_id
     "get_ingredients_by_course": "SELECT i.i_id, i_name, available FROM ingredient AS i INNER JOIN course_ingredient AS ci ON i.i_id=ci.i_id WHERE c_id={c_id}",
@@ -18,7 +18,19 @@ get_queries = {
     "get_allergenes_by_ingredient": "SELECT a.a_id, a.a_name FROM allergene AS a INNER JOIN ingredient_allergene AS ia ON a.a_id=ia.a_id WHERE ia.i_id={i_id}",
 
     # Get all allergenes
-    "get_allergenes": "SELECT a_id, a_name FROM allergene"
+    "get_allergenes": "SELECT a_id, a_name FROM allergene",
+
+    # Get all categories
+    "get_categories": "SELECT ca_id, ca_name FROM category",
+
+    # Get all selection categories
+    "get_selection_categories": "SELECT sc_id, sc_name FROM selection_category",
+
+    # Get all selections
+    "get_selections": "SELECT s_id, s_name, sc_id, i_id FROM selection",
+
+    # Get selections by course
+    "get_selections_by_course": "SELECT s.s_id, s_name, s.sc_id, s.i_id FROM selection AS s INNER JOIN course_selection as cs ON s.s_id=cs.s_id WHERE cs.c_id={c_id}"
 }
 
 def get_courses(db):
@@ -27,10 +39,12 @@ def get_courses(db):
 
     try:
         cur.execute(get_queries["get_courses"])
-        for (c_id, c_name, price) in cur:
+        for (c_id, c_name, ca_id, info, price) in cur:
             courses.append({
                 "c_id": str(c_id),
                 "c_name": str(c_name),
+                "ca_id": str(ca_id),
+                "info": str(info),
                 "price": str(price),
                 "ingredients": []
             })
@@ -81,6 +95,56 @@ def get_allergenes(db):
         cur.close()
 
 
+def get_categories(db):
+    cur = db.cursor()
+    categories = []
+
+    try:
+        cur.execute(get_queries["get_categories"])
+        for (ca_id, ca_name) in cur:
+            categories.append({
+                "ca_id": str(ca_id),
+                "ca_name": str(ca_name)
+            })
+        return categories
+    finally:
+        cur.close()
+
+
+def get_selection_categories(db):
+    cur = db.cursor()
+    categories = []
+
+    try:
+        cur.execute(get_queries["get_selection_categories"])
+        for (sc_id, sc_name) in cur:
+            categories.append({
+                "sc_id": str(sc_id),
+                "sc_name": str(sc_name)
+            })
+        return categories
+    finally:
+        cur.close()
+
+
+def get_selections(db):
+    cur = db.cursor()
+    selections = []
+
+    try:
+        cur.execute(get_queries["get_selections"])
+        for (s_id, s_name, sc_id, i_id) in cur:
+            selections.append({
+                "s_id": str(s_id),
+                "s_name": str(s_name),
+                "sc_id": str(sc_id),
+                "i_id": str(i_id)
+            })
+        return selections
+    finally:
+        cur.close()
+
+
 def get_ingredients_by_course(db, c_id):
     cur = db.cursor()
     ingredients = []
@@ -121,6 +185,30 @@ def get_allergenes_by_ingredient(db, i_id):
                 "a_name": str(a_name)
             })
         return allergenes
+    except (Error) as err:
+        if 'Unknown column' in str(err):
+            return INVALID_TYPE_EXCEPTION
+        raise err
+    finally:
+        cur.close()
+
+
+def get_selections_by_course(db, c_id):
+    cur = db.cursor()
+    selections = []
+
+    try:
+        if c_id == None:
+            return EMPTY_INPUT_EXCEPTION
+        cur.execute(get_queries["get_selections_by_course"].replace("{c_id}", str(c_id)))
+        for (s_id, s_name, sc_id, i_id) in cur:
+            selections.append({
+                "s_id": str(s_id),
+                "s_name": str(s_name),
+                "sc_id": str(sc_id),
+                "i_id": str(i_id)
+            })
+        return selections
     except (Error) as err:
         if 'Unknown column' in str(err):
             return INVALID_TYPE_EXCEPTION
--- test/test_get_functions.py
+++ test/test_get_functions.py
@@ -10,10 +10,14 @@ from mysql.connector import errorcode, IntegrityError, DataError, Error
 # Magnus Steinstø
 
 expected_return_values = {
-    "get_courses": "[{'c_id': '1', 'c_name': 'course alpha', 'price': '5.20', 'ingredients': [{'i_id': '1', 'i_name': 'ingredient alpha', 'available': '1', 'allergenes': [{'a_id': '2', 'a_name': 'allergene bravo'}, {'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '3', 'i_name': 'ingredient charlie', 'available': '1', 'allergenes': []}]}, {'c_id': '2', 'c_name': 'course bravo', 'price': '4.20', 'ingredients': [{'i_id': '1', 'i_name': 'ingredient alpha', 'available': '1', 'allergenes': [{'a_id': '2', 'a_name': 'allergene bravo'}, {'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '2', 'i_name': 'ingredient bravo', 'available': '1', 'allergenes': [{'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '5', 'i_name': 'ingredient echo', 'available': '1', 'allergenes': []}]}, {'c_id': '3', 'c_name': 'course charlie', 'price': '3.75', 'ingredients': [{'i_id': '1', 'i_name': 'ingredient alpha', 'available': '1', 'allergenes': [{'a_id': '2', 'a_name': 'allergene bravo'}, {'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '3', 'i_name': 'ingredient charlie', 'available': '1', 'allergenes': []}]}, {'c_id': '4', 'c_name': 'course delta', 'price': '2.10', 'ingredients': [{'i_id': '1', 'i_name': 'ingredient alpha', 'available': '1', 'allergenes': [{'a_id': '2', 'a_name': 'allergene bravo'}, {'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '5', 'i_name': 'ingredient echo', 'available': '1', 'allergenes': []}]}, {'c_id': '5', 'c_name': 'course echo', 'price': '7.40', 'ingredients': [{'i_id': '1', 'i_name': 'ingredient alpha', 'available': '1', 'allergenes': [{'a_id': '2', 'a_name': 'allergene bravo'}, {'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '2', 'i_name': 'ingredient bravo', 'available': '1', 'allergenes': [{'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '3', 'i_name': 'ingredient charlie', 'available': '1', 'allergenes': []}]}]",
+    "get_courses": "[{'c_id': '1', 'c_name': 'course alpha', 'ca_id': '1', 'info': 'info alpha', 'price': '5.20', 'ingredients': [{'i_id': '1', 'i_name': 'ingredient alpha', 'available': '1', 'allergenes': [{'a_id': '2', 'a_name': 'allergene bravo'}, {'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '3', 'i_name': 'ingredient charlie', 'available': '1', 'allergenes': []}]}, {'c_id': '2', 'c_name': 'course bravo', 'ca_id': '2', 'info': 'info bravo', 'price': '4.20', 'ingredients': [{'i_id': '1', 'i_name': 'ingredient alpha', 'available': '1', 'allergenes': [{'a_id': '2', 'a_name': 'allergene bravo'}, {'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '2', 'i_name': 'ingredient bravo', 'available': '1', 'allergenes': [{'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '5', 'i_name': 'ingredient echo', 'available': '1', 'allergenes': []}]}, {'c_id': '3', 'c_name': 'course charlie', 'ca_id': '1', 'info': 'info charlie', 'price': '3.75', 'ingredients': [{'i_id': '1', 'i_name': 'ingredient alpha', 'available': '1', 'allergenes': [{'a_id': '2', 'a_name': 'allergene bravo'}, {'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '3', 'i_name': 'ingredient charlie', 'available': '1', 'allergenes': []}]}, {'c_id': '4', 'c_name': 'course delta', 'ca_id': '3', 'info': 'info delta', 'price': '2.10', 'ingredients': [{'i_id': '1', 'i_name': 'ingredient alpha', 'available': '1', 'allergenes': [{'a_id': '2', 'a_name': 'allergene bravo'}, {'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '5', 'i_name': 'ingredient echo', 'available': '1', 'allergenes': []}]}, {'c_id': '5', 'c_name': 'course echo', 'ca_id': '4', 'info': 'info echo', 'price': '7.40', 'ingredients': [{'i_id': '1', 'i_name': 'ingredient alpha', 'available': '1', 'allergenes': [{'a_id': '2', 'a_name': 'allergene bravo'}, {'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '2', 'i_name': 'ingredient bravo', 'available': '1', 'allergenes': [{'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '3', 'i_name': 'ingredient charlie', 'available': '1', 'allergenes': []}]}]",
     "get_ingredients": "[{'i_id': '1', 'i_name': 'ingredient alpha', 'available': '1', 'allergenes': [{'a_id': '2', 'a_name': 'allergene bravo'}, {'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '2', 'i_name': 'ingredient bravo', 'available': '1', 'allergenes': [{'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '3', 'i_name': 'ingredient charlie', 'available': '1', 'allergenes': []}, {'i_id': '4', 'i_name': 'ingredient delta', 'available': '1', 'allergenes': []}, {'i_id': '5', 'i_name': 'ingredient echo', 'available': '1', 'allergenes': []}]",
     "get_ingredients_by_course": "[{'i_id': '1', 'i_name': 'ingredient alpha', 'available': '1', 'allergenes': [{'a_id': '2', 'a_name': 'allergene bravo'}, {'a_id': '3', 'a_name': 'allergene charlie'}]}, {'i_id': '3', 'i_name': 'ingredient charlie', 'available': '1', 'allergenes': []}]",
-    "get_allergens_by_ingredient": "[{'a_id': '2', 'a_name': 'allergene bravo'}, {'a_id': '3', 'a_name': 'allergene charlie'}]"
+    "get_allergens_by_ingredient": "[{'a_id': '2', 'a_name': 'allergene bravo'}, {'a_id': '3', 'a_name': 'allergene charlie'}]",
+    "get_categories": "[{'ca_id': '1', 'ca_name': 'category alpha'}, {'ca_id': '2', 'ca_name': 'category bravo'}, {'ca_id': '3', 'ca_name': 'category charlie'}, {'ca_id': '4', 'ca_name': 'category delta'}]",
+    "get_selection_categories": "[{'sc_id': '1', 'sc_name': 'selection category alpha'}, {'sc_id': '2', 'sc_name': 'selection category bravo'}]",
+    "get_selections": "[{'s_id': '1', 's_name': 'selection alpha', 'sc_id': '1', 'i_id': 'None'}, {'s_id': '2', 's_name': 'selection bravo', 'sc_id': '2', 'i_id': '2'}, {'s_id': '3', 's_name': 'selection charlie', 'sc_id': '1', 'i_id': '1'}, {'s_id': '4', 's_name': 'selection delta', 'sc_id': '1', 'i_id': '1'}]",
+    "get_selections_by_course": "[{'s_id': '1', 's_name': 'selection alpha', 'sc_id': '1', 'i_id': 'None'}, {'s_id': '2', 's_name': 'selection bravo', 'sc_id': '2', 'i_id': '2'}]"
 }
 
 def get_db():
@@ -48,6 +52,33 @@ class TestGetFunctions(unittest.TestCase):
         # Get values and compare to expected results
         ingredients = get_ingredients(db)
         self.assertEqual(str(ingredients), expected_return_values["get_ingredients"])
+        db.close()
+
+    
+    def test_get_categories(self):
+        db = get_db()
+
+        # Get values and compare to expected results
+        categories = get_categories(db)
+        self.assertEqual(str(categories), expected_return_values["get_categories"])
+        db.close()
+
+
+    def test_get_selection_categories(self):
+        db = get_db()
+
+        # Get values and compare to expected results
+        categories = get_selection_categories(db)
+        self.assertEqual(str(categories), expected_return_values["get_selection_categories"])
+        db.close()
+
+
+    def test_get_selections(self):
+        db = get_db()
+
+        # Get values and compare to expected results
+        selections = get_selections(db)
+        self.assertEqual(str(selections), expected_return_values["get_selections"])
         db.close()
 
 
@@ -87,6 +118,26 @@ class TestGetFunctions(unittest.TestCase):
 
         # Get with empty id value
         self.assertEqual(get_allergenes_by_ingredient(db, None),
+                         EMPTY_INPUT_EXCEPTION)
+        db.close()
+
+
+    def test_get_selections_by_course(self):
+        db = get_db()
+
+        # Get values and compare to expected results
+        selections = get_selections_by_course(db, 1)
+        self.assertEqual(str(selections), expected_return_values["get_selections_by_course"])
+
+        # Get with non-existing id (should return empty set)
+        self.assertEqual(get_selections_by_course(db, 999), [])
+
+        # Get with invalid input id
+        self.assertEqual(get_selections_by_course(db, "a"),
+                         INVALID_TYPE_EXCEPTION)
+
+        # Get with empty id value
+        self.assertEqual(get_selections_by_course(db, None),
                          EMPTY_INPUT_EXCEPTION)
         db.close()
 
